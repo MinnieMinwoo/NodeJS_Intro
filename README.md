47d0abdc192c4714789e894d6b03558770f234a1
MinnieMinwoo <nhs075241@gmail.com>
2023-01-11 14:11:21 +0900

    make basic web server

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# HTML_Study
+# NodeJS Study
 
-Source:  
-https://nihilog.github.io/post/2021-05-02-01-complete-html-guide  
-https://www.opentutorials.org/course/3084
+Source:
--- main.js
+++ main.js
@@ -1,16 +1,60 @@
-var http = require("http");
-var fs = require("fs");
-var app = http.createServer(function (request, response) {
-    var url = request.url;
-    if (request.url == "/") {
-        url = "/index.html";
-    }
-    if (request.url == "/favicon.ico") {
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const path = require("path");
+
+function setTemplate(title, description, list) {
+    const template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+    return template;
+}
+
+const app = http.createServer(function (request, response) {
+    const _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
+    let list = `<ul>`;
+    fs.readdir("./data", function (error, filelist) {
+        for (let i = 0; i < filelist.length; i++) {
+            list =
+                list +
+                `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        }
+        list = list + "</ul>";
+        if (pathname === "/") {
+            if (queryData.id === undefined) {
+                const title = "Welcome";
+                const description = "Hello, Node.js";
+                const template = setTemplate(title, description, list);
+                response.writeHead(200);
+                response.end(template);
+            } else {
+                `nodejs/${queryData.id}`,
+                    "utf8",
+                    (err, description) => {
+                        const title = queryData.id;
+                        const template = setTemplate(title, description, list);
+                        response.writeHead(200);
+                        response.end(template);
+                    };
+            }
+        } else {
+            response.writeHead(404);
+            response.end("Not found");
+        }
+    });
 });
 app.listen(3000);
